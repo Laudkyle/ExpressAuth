24dbe0ccbb5393715d599a9dc768e4297d4099a3
Laudkyle <kyleaby1@gmail.com>
2024-09-30 19:28:37 +0000

    utils.tokens done

---
+++ utils/token.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+const access = process.env.JWT_ACCESS_SECRET;
+const refresh = process.env.JWT_REFRESH_SECRET;
+
+export const generateAccessToken = (user) => {
+  return jwt.sign({ userId: user._id }, access, { expiresIn: "15m" });
+};
+
+export const generateRefreshToken = (user) => {
+  return jwt.sign({ userId: user._id }, refresh, { expiresIn: "7d" });
+};
