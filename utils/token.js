417c7ff4aa0e931e4e1cd59edee231db2ce05bef
Laudkyle <kyleaby1@gmail.com>
2024-09-30 19:34:02 +0000

    jwt access and refresh created successfully

--- utils/token.js
+++ utils/token.js
@@ -3,4 +3,9 @@ import jwt from JsonWebTokenError
 const access = process.env.JWT_ACCESS_SECRET
 const refresh = process.env.JWT_REFRESH_SECRET
 
-const generateAccessToken = 
+const generateAccessToken = (user)=>{
+    return jwt.sign({userId:user._id},access,{expiresIn:'15m'})
+}
+const generateRefreshToken = (user)=>{
+    return jwt.sign({userId:user._id},refresh,{expiresIn:'7d'})
+}
