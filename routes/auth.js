35fe16db473f0c75d6076d4183f3a3652d8c414e
Laudkyle <kyleaby1@gmail.com>
2024-09-30 19:58:28 +0000

    login completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User";
-
+import { generateAcessToken, generateRefreshToken } from "../utils/token";
+import jwt from "jsonWebToken";
 const router = express.Router();
 router.use(express.json());
 
@@ -14,5 +15,29 @@ router.post("/register", async (req, res) => {
     user = new User({ username, password });
     await user.save();
     res.status(201).json({ msg: "User Created Succesfully" });
-  } catch (error) {res.status(500).json({err: "Something went Wrong"})}
+  } catch (error) {
+    res.status(500).json({ err: "Something went Wrong" });
+  }
 });
+
+
+router.post('/login',async (req,res)=>{
+    const {username,password} = req.body
+
+    try {
+        let user = await User.findOne({username});
+        if(!user){
+            return res.status(403).json({msg:'Invalid Credentails'})
+        }
+        const isMatch = await user.isValidPassword(password);
+        if (!isMatch){
+            return res.status(403).json({msg:'Invalid Credentails'})
+
+        }
+        const accessToken = generateAcessToken(user)
+        const refreshToken = generateRefreshToken(user)
+        res.status(200).json({access: accessToken,refresh:refreshToken})
+    } catch (error) {
+        res.status(500).json({msg: "Something went wrong during the authentication process"})
+    }
+})
