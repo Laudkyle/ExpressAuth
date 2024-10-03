42f43b026475363752e0f68196c4285e88443452
Laudkyle <kyleaby1@gmail.com>
2024-10-03 05:09:21 +0000

    checking if token is present

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,11 @@
 import jwt from 'jsonwebtoken'
 
+
+const accessToken = process.env.JWT_ACCESS_SECRET
 const auth = (req,res,next)=>{
+    token = req.header('Authorization').replace('Bearer','')
+    if (!token){
+        res.status(403).json({msg:"You need an access token to interact with this API"})
+    }
     
 }
