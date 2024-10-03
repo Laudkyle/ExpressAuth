51a6136edff91b9118e15fe2afd4ec07d1238ed5
Laudkyle <kyleaby1@gmail.com>
2024-10-03 05:13:13 +0000

    auth middleware created

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,10 +2,19 @@ import jwt from 'jsonwebtoken'
 
 
 const accessToken = process.env.JWT_ACCESS_SECRET
+
 const auth = (req,res,next)=>{
     token = req.header('Authorization').replace('Bearer','')
     if (!token){
         res.status(403).json({msg:"You need an access token to interact with this API"})
     }
+try {
     
+    const decoded = jwt.verify(token,accessToken)
+    req.user = decoded
+    res.sendStatus(200)
+    next()
+} catch (error) {
+    res.status(403).json({error:'Invalid access token'})
+}
 }
